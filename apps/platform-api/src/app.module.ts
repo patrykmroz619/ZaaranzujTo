0114feb67db577c4fe64a721d11b578c7ac0cd8e
Patryk Mróz <patrykmroz619@wp.pl>
2026-03-07 16:41:21 +0100

    fix: building of nest js app

--- apps/platform-api/src/app.module.ts
+++ apps/platform-api/src/app.module.ts
@@ -1,24 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ConfigurationModule } from './config/config.module';
-import { DatabaseModule } from './database/database.module';
-import { IdentityAccessModule } from './modules/identity-access/identity-access.module';
-import { ProfileModule } from './modules/profile/profile.module';
-import { UsersModule } from './modules/users/users.module';
-import { CurriculumModule } from './modules/curriculum/curriculum.module';
-import { LearningProgressModule } from './modules/learning-progress/learning-progress.module';
-import { TaskVerificationModule } from './modules/task-verification/task-verification.module';
+import { Module } from "@nestjs/common";
+import { ConfigurationModule } from "./config/config.module";
+import { DatabaseModule } from "./database/database.module";
+import { ProfileModule } from "./modules/profile/profile.module";
+import { UsersModule } from "./modules/users/users.module";
 
 @Module({
-  imports: [
-    ConfigurationModule,
-    DatabaseModule,
-    IdentityAccessModule,
-    UsersModule,
-    ProfileModule,
-    CurriculumModule,
-    LearningProgressModule,
-    TaskVerificationModule,
-  ],
+  imports: [ConfigurationModule, DatabaseModule, UsersModule, ProfileModule],
   controllers: [],
   providers: [],
 })
